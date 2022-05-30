1dc9f3c19db4d4a91e38d60e5f09d65dbee86552
Matt <mtthw8198@gmail.com>
2022-05-29 20:33:18 -0700

    tweaked compile script

--- java/scripts/compile.sh
+++ java/scripts/compile.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
-export PATH=$JAVA_HOME/bin:$PATH
+#For use when compiling on Lab Machines
+DIR="$(pwd)"
+echo $DIR
+
+mkdir $DIR/java/classes
+
+#export JAVA_HOME=/usr/csshare/pkgs/jdk1.7.0_17
+#export PATH=$JAVA_HOME/bin:$PATH
 
 # compile the java program
-javac -d $DIR/../classes $DIR/../src/Cafe.java
+javac -d $DIR/java/classes $DIR/java/src/Cafe.java
 
 #run the java program
 #Use your database name, port number and login
-java -cp $DIR/../classes:$DIR/../lib/pg73jdbc3.jar Cafe $USER"_DB" $PGPORT $USER
+java -cp $DIR/java/classes:$DIR/java/lib/pg73jdbc3.jar Cafe ${1} ${2} ${3}
+
+sleep 10
 

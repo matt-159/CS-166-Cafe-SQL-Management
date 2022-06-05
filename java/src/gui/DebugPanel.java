04fbd5e7ccd7831cd8bf9f2ea5f526cf41672db5
matt-159 <mtthw8198@gmail.com>
2022-06-05 00:07:37 -0700

    set debug panel to use a monowidth font

--- java/src/gui/DebugPanel.java
+++ java/src/gui/DebugPanel.java
@@ -3,17 +3,22 @@ package gui;
 import util.TextAreaOutputStream;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.PrintStream;
 
 public class DebugPanel extends JFrame {
 
+    JScrollPane debugScrollPane;
     JTextArea debugOuput;
 
     public DebugPanel() {
         super("Debug Output");
 
         debugOuput = new JTextArea();
-        this.add(debugOuput);
+        debugOuput.setFont(new Font("Consolas", Font.PLAIN, 12));
+        debugScrollPane = new JScrollPane(debugOuput);
+
+        this.add(debugScrollPane);
 
         this.setSize(800, 600);
         this.setVisible(true);
